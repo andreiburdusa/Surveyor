8c022ac85d27d92f8db16fd2eb72cdcc2b6bc4b9
Julian Sutherland <julian.sutherland10@imperial.ac.uk>
2025-02-11 22:23:04 +0000

    Updated proofs to be generated with newest version

--- examples/Lean/R1CSDemo/Ex1.lean
+++ examples/Lean/R1CSDemo/Ex1.lean
@@ -4,20 +4,23 @@ import Mathlib.Data.ZMod.Basic
 namespace Ex1
 
 def p : ℕ := 21888242871839275222246405745257275088548364400416034343698204186575808495617
-instance : Fact p.Prime := sorry
+axiom p_prime : p.Prime
+instance : Fact p.Prime := by rw [fact_iff]; exact p_prime
 
 def Witness : Type := Fin 4 → ZMod p
 
 def spec (w : Witness) : Prop := w 1 = w 2 * w 3
 
+
 def circuit (w : Witness) : Prop :=
     (21888242871839275222246405745257275088548364400416034343698204186575808495616 * (w 2)) * (1 * (w 3)) = 21888242871839275222246405745257275088548364400416034343698204186575808495616 * (w 1)
 
-example : ∀ w : Witness, circuit w → spec w := by
+example : ∀ w : Witness, w 0 = 1 → circuit w → spec w := by
     unfold circuit spec
     have : (21888242871839275222246405745257275088548364400416034343698204186575808495616 : ZMod p) = -1 := by decide
     rw [this]
-    intros w h
+    intros w _ h
     aesop
+
 
 end Ex1
--- examples/Lean/R1CSDemo/IsZero.lean
+++ examples/Lean/R1CSDemo/IsZero.lean
@@ -5,9 +5,7 @@ namespace IsZero
 
 def p : ℕ := 21888242871839275222246405745257275088548364400416034343698204186575808495617
 axiom p_prime : p.Prime
-instance : Fact p.Prime := by
- rw [fact_iff]
- exact p_prime
+instance : Fact p.Prime := by rw [fact_iff]; exact p_prime
 
 def Witness : Type := Fin 4 → ZMod p
 
@@ -17,18 +15,18 @@ def circuit (w : Witness) : Prop :=
     (1 * (w 2)) * (1 * (w 3)) = 1 * (w 0) + 21888242871839275222246405745257275088548364400416034343698204186575808495616 * (w 1) ∧
     (1 * (w 2)) * (1 * (w 1)) = 0
 
-example : ∀ w : Witness, w 0 = 1 → (circuit w ↔ spec w) := by
+example : ∀ w : Witness, w 0 = 1 → circuit w → spec w := by
     unfold circuit spec
     intros w w0_is_1
-    simp only [w0_is_1, one_mul, zero_add]
     have : (21888242871839275222246405745257275088548364400416034343698204186575808495616 : ZMod p) = -1 := by decide
     rw [this]
-    simp only [neg_mul, one_mul, mul_eq_zero, ne_eq]
-    apply Iff.intro
-    · rintro ⟨h₀, h₁ | h₁⟩ <;> have h₀ := add_eq_of_eq_add_neg h₀
-      · rw [h₁, zero_mul, zero_add] at h₀
-        aesop
-      · aesop
+    simp only [w0_is_1, one_mul, zero_add, neg_mul]
+    intros h
+    by_cases w2_is_0 : w 2 = 0
+    · simp only [w2_is_0, zero_mul, and_true] at h
+      have h := add_eq_of_eq_add_neg h
+      rw [zero_add] at h
+      left; exact ⟨w2_is_0, h⟩
     · aesop
 
 end IsZero
